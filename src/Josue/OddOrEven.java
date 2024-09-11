e0b437b320536c65b894a68b8a3ae5b14ce4a820
josue <jpbusientre@gmail.com>
2024-09-11 14:58:28 -0400

    Added solution

---
+++ src/Josue/OddOrEven.java
@@ -0,0 +1,16 @@
+package Josue;
+
+public class OddOrEven {
+
+
+
+
+
+
+       /*Write  a method which can identify given int number is even or odd
+    Output ex:
+    identify(5); ->"Odd"
+    identify(6); ->"Even"*/
+
+    //solution added
+}
