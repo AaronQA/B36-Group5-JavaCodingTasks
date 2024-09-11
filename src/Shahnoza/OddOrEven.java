5fb5e9bf7cabe85c4dccc111394fcbfc6dee3ef3
Shahnoza <s_kholova@hotmail.com>
2024-09-11 13:58:29 -0500

    added solution on QO

---
+++ src/Shahnoza/OddOrEven.java
@@ -0,0 +1,10 @@
+package Shahnoza;
+
+public class OddOrEven {
+       /*Write  a method which can identify given int number is even or odd
+    Output ex:
+    identify(5); ->"Odd"
+    identify(6); ->"Even"*/
+
+    // solution added
+}
