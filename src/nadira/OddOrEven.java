32b8fbe54ce754be4945765f079a1adc134ac8e1
NadiraA <hralakz@gmail.com>
2024-09-16 12:08:49 -0400

    solution1

--- src/nadira/OddOrEven.java
+++ src/nadira/OddOrEven.java
@@ -4,7 +4,7 @@ public class OddOrEven {
     public static void main(String[] args) {
 
 
-        System.out.println("identify(5) = " + identify(5));
+        System.out.println(identify(7));
     }
        /*Write  a method which can identify given int number is even or odd
     Output ex:
@@ -12,17 +12,19 @@ public class OddOrEven {
     identify(6); ->"Even"*/
 
 
-
-
     public static String identify(int number) {
-        if (number % 2 == 0) {
-            return "Even";
+
+        if (number >= 0) {
+
+            if (number % 2 == 0) {
+                return "Even";
+
+            }else{
+                return "Odd";
+            }
+
 
         }
-        if (number % 2 != 0) {
-            return "Odd";
-        }
-        return "invalid input, please enter a number";
-
+        return "invalid input, please enter a positive number";
     }
 }
