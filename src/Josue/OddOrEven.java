8360f08bcef2814d39824aca5575b03ede5a681d
josue <jpbusientre@gmail.com>
2024-09-18 14:48:09 -0400

    Added OddOrEven method

--- src/Josue/OddOrEven.java
+++ src/Josue/OddOrEven.java
@@ -1,12 +1,27 @@
 package Josue;
 
+import java.util.Scanner;
+
 public class OddOrEven {
 
-    public static void main(String[] args) {
+    public static String oddOrEven(int n) {
+        String result = "";
+        if( n % 2 == 0){
+            result = "Even";
+        }else{
+            result = "Odd";
+        }
 
-
+        return result;
 
     }
+
+    public static void main(String[] args) {
+        System.out.println(oddOrEven(3));
+    }
+
+    }
+
 
 
 
@@ -17,4 +32,4 @@ public class OddOrEven {
     identify(6); ->"Even"*/
 
     //solution added
-}
+
