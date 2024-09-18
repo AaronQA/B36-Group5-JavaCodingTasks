61e85e43c42780cfa3d5e58ff60379bdb0de98cf
shahnoza83 <s_kholova@hotmail.com>
2024-09-18 14:30:52 -0500

    updated solution on QO

--- src/Shahnoza/OddOrEven.java
+++ src/Shahnoza/OddOrEven.java
@@ -7,8 +7,8 @@ public class OddOrEven {
     identify(6); ->"Even"*/
 
     public static void main(String[] args) {
-        System.out.println(oddEven(5));
-        System.out.println(oddEven(6));
+        System.out.println(oddEven(-6));
+        System.out.println(oddEven(0));
     }
     public static String oddEven(int num) {
         if(num%2==0){
