cd44197382439c58dd56e23171f6c55fe4ee5065
Gulnaz <raketagoi89@gmail.com>
2024-09-19 13:12:48 -0400

    Added some changes

--- src/Gulnaz/OddOrEven.java
+++ src/Gulnaz/OddOrEven.java
@@ -2,7 +2,7 @@ package Gulnaz;
 
 public class OddOrEven {
     public static void main(String[] args) {
-
+        System.out.println(identify(7));
     }
 
        /*Write  a method which can identify given int number is even or odd
@@ -11,4 +11,19 @@ public class OddOrEven {
     identify(6); ->"Even"*/
 
     //solution all done
+    public static String identify(int number) {
+
+        if (number >= 0) {
+
+            if (number % 2 == 0) {
+                return "Even";
+
+            }else{
+                return "Odd";
+            }
+
+
+        }
+        return "invalid input, please enter a positive number";
+    }
 }
