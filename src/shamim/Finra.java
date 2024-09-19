7af62d806ca88e2bdf61369a89febd9b56159bd2
sj <sham19.jab@gmail.com>
2024-09-19 18:26:00 -0400

    done

--- src/shamim/Finra.java
+++ src/shamim/Finra.java
@@ -5,5 +5,43 @@ public class Finra {
     //print "FIN" instead of the number and
     //for numbers which are a multiple of 5, print "RA" instead of the number.
     //for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+    public static void main(String[] args) {
+        finra();
+    }
+    public static void finra (){
+        String result = "";
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result += "FINRA ";
+            }else if (i% 5 == 0) {
+                result += "RA ";
+            }else if (i % 3 == 0) {
+                result += "FIN ";
+                } else {
+                result += i + " ";
 
-}
+                System.out.print("result)");
+
+
+            }
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
