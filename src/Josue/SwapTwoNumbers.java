fbbe65950d28bcb01f32a011408b4e56eb6c43ad
josue <jpbusientre@gmail.com>
2024-09-25 16:18:38 -0400

    completed swap two numbers

---
+++ src/Josue/SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package Josue;
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("Num1 original = 5 after swap = " + num1 + "\n Num2 original = 10 after swap = " + num2);
+
+    }
+
+    //Swap two int variables' values without using a third variable
+
+}
