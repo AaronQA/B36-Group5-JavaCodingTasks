08c51945fe99fe16230e7005fb1fdc6faca5c578
josue <jpbusientre@gmail.com>
2024-09-25 16:23:13 -0400

    completed swap two numbers

--- src/Josue/SwapTwoNumbers.java
+++ src/Josue/SwapTwoNumbers.java
@@ -4,8 +4,8 @@ public class SwapTwoNumbers {
 
     public static void main(String[] args) {
 
-        int num1 = 5;
-        int num2 = 10;
+        int num1 = 10;
+        int num2 = 15;
 
         num1 = num1 + num2;
         num2 = num1 - num2;
