e9e312be2d0e9df9f403a918d10e9f52be1cae7b
shahnoza83 <s_kholova@hotmail.com>
2024-10-02 11:34:21 -0500

    added SwapTwoNumbers solution

---
+++ src/Shahnoza/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+package Shahnoza;
+
+public class SwapTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        System.out.println("first a: " + a);
+        System.out.println("first b: " + b);
+
+        a = a + b; //a is 15 now
+        b = a - b; //b is 5 now
+        a = a - b; //a is 10 now
+
+        System.out.println("new a: " + a);//new a: 10
+        System.out.println("new b: " + b);//new b: 5
+
+    }
+}
+
