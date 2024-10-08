1361599305170c25d02d572065c8c4a9045d0290
Gulnaz <raketagoi89@gmail.com>
2024-10-08 16:43:04 -0400

    reversedNegativeNumber by Nadira

--- src/Gulnaz/reverseNegativeNumber.java
+++ src/Gulnaz/reverseNegativeNumber.java
@@ -4,16 +4,20 @@ public class reverseNegativeNumber {
     //Write a return method that can reverse digits of a negative number and return it as int
     //input: -35  output: -53
 
-    public static int reversedNegativeNumber(int num){
-        int reversedNegativeNumber=0;
-        while(num !=0){ //50; 5
-            int digit= num%10;   //50%10=0; 5%10=5
-            reversedNegativeNumber=reversedNegativeNumber*10+digit;//0*10+0; 0+5
-            num/=10; //50/10=5; 5/10=0
+   //reverseNegativeNumber by Nadira
+    public static int reverseNegativeNumber(int num){
+        int reverseNegativeNumber=0;
+        while(num!=0){
+            int digit= num%10;
+            reverseNegativeNumber=reverseNegativeNumber*10+digit;
+            num/=10;
         }
-        return reversedNegativeNumber;
+        return reverseNegativeNumber;
     }
 
+    public static void main(String[] args) {
+        System.out.println(reverseNegativeNumber(-35));
+    }
 
 }
 
