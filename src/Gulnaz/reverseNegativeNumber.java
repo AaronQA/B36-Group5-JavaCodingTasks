70e169e05c7efa1efc4c51f3763044803fc4c557
Gulnaz <raketagoi89@gmail.com>
2024-10-09 01:51:30 -0400

    reversedNegativeNumber was updated by adding comment lines with explanations.

--- src/Gulnaz/reverseNegativeNumber.java
+++ src/Gulnaz/reverseNegativeNumber.java
@@ -4,7 +4,65 @@ public class reverseNegativeNumber {
     //Write a return method that can reverse digits of a negative number and return it as int
     //input: -35  output: -53
 
-   //reverseNegativeNumber by Tilbe
+    //1. The method `reversedNegativeNumber` takes an integer `num` as a parameter.
+    public static int reverseNegativeNumber(int num){
+
+        //2. It initializes a variable `reversedNegativeNumber` to 0,
+        // which will store the reversed digits.
+        int reverseNegativeNumber=0;
+
+        //3. The method enters a while loop that continues until `num` becomes 0.
+        while(num!=0){
+
+            //4. Inside the loop, it calculates the last digit of `num`
+            // by taking the remainder of `num` divided by 10 (`num % 10`).
+            int digit= num%10;
+
+            //5. It then updates `reversedNegativeNumber` by multiplying it
+            // by 10 and adding the last digit (`reversedNegativeNumber * 10 + digit`).
+            reverseNegativeNumber=reverseNegativeNumber*10 + digit;
+
+            //6. Finally, it updates `num` by dividing it
+            // by 10 (`num /= 10`) to remove the last digit from `num`.
+            num/=10;
+        }
+
+        //7. Once the loop finishes, the method returns `reversedNegativeNumber`,
+        // which contains the reversed digits of the negative number.
+        return reverseNegativeNumber;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseNegativeNumber(-35));
+    }
+
+
+                    //reverseNegativeNumber by (:Nadira(:
+
+//    public static int reverseNegativeNumber(int num){
+//        int reverseNegativeNumber=0;
+//        while(num!=0){ //50
+//            int digit= num%10; //digit=0
+//            reverseNegativeNumber=reverseNegativeNumber*10 + digit;//""+0+0
+//            num/=10;
+//        }
+//        return reverseNegativeNumber;
+//    }
+
+                    //reverseNegativeNumber by (:Barno(:
+//    public static int reverseNegativeNumber(int num){
+//        int reverseNegativeNumber=0;
+//        while(num!=0){ //50
+//            int digit= num%10; //digit=0
+//            reverseNegativeNumber=reverseNegativeNumber*10 + digit;//""+0+0
+//            num/=10;
+//        }
+//        return reverseNegativeNumber;
+//    }
+
+
+
+                     // reverseNegativeNumber by (:Tilbe(:
 //    public static int reverseNegativeNumber(int num){
 //        int reverseNegativeNumber=0;
 //        if(num>0) throw new IllegalArgumentException("Number must be less than 0");
@@ -16,25 +74,27 @@ public class reverseNegativeNumber {
 //        }
 //        return reverseNegativeNumber;
 //    }
-    //reverseNegativeNumber by Aiturgan
-    public static int reverseNegativeNumber(int num){
-        int reverseNegativeNumber=0;
-        while(num!=0){ //50
-            int digit= num%10; //digit=0
-            reverseNegativeNumber=reverseNegativeNumber*10 + digit;//""+0+0
-            num/=10;
-        }
-        return reverseNegativeNumber;
-    }
+
+
+
+
+                         //reverseNegativeNumber by (:Aiturgan(:
+//    public static int reverseNegativeNumber(int num){
+//        int reverseNegativeNumber=0;
+//        while(num!=0){
+//            int digit= num%10;
+//            reverseNegativeNumber=reverseNegativeNumber*10 + digit;
+//            num/=10;
+//        }
+//        return reverseNegativeNumber;
+//    }
 
 
 
 
 
 
-    public static void main(String[] args) {
-        System.out.println(reverseNegativeNumber(-50));
-    }
+
 /*
 The selected code is a method named `reversedNegativeNumber` that takes an integer as input and returns the reversed digits of the negative number as an integer.
 
