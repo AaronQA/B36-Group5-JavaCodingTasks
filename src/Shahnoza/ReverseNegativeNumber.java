bb7b904e0769e44fd2d0dff0fc6e96e85b9acb23
shahnoza83 <s_kholova@hotmail.com>
2024-10-08 20:30:33 -0500

    added changes to  ReverseNegativeNumber

--- src/Shahnoza/ReverseNegativeNumber.java
+++ src/Shahnoza/ReverseNegativeNumber.java
@@ -1,16 +1,27 @@
 package Shahnoza;
 
+
 public class ReverseNegativeNumber {
     //Write a return method that can reverse digits of a negative number and return it as int
     //input: -35  output: -53
 
     public static int reverseNegativeNumber(int num) {
         int reversedNum = 0;
+        if ( num>0){
+            throw new IllegalArgumentException("Invalid negative number");
+        }
         while (num != 0) {
             int remainder = num % 10;
             reversedNum = reversedNum * 10 + remainder;
             num /= 10;
         }
-        return -reversedNum;
+        return reversedNum;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseNegativeNumber(-35));
+        System.out.println(reverseNegativeNumber(-105));
+        System.out.println(reverseNegativeNumber(56));
     }
 }
