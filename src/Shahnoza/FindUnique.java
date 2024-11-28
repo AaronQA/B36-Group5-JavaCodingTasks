bf8516a266f89218b430ff2d5bb73ba453464522
shahnoza83 <s_kholova@hotmail.com>
2024-11-27 20:25:50 -0600

    made correction on FindUnique task

--- src/Shahnoza/FindUnique.java
+++ src/Shahnoza/FindUnique.java
@@ -6,29 +6,33 @@ public class FindUnique {
       Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";*/
 
     public static void main(String[] args) {
-
-        System.out.println(findUnique("AAABBBCCCDEF"));
-        System.out.println(findUnique("AAABBBCCCDEF234"));
         System.out.println(findUnique("      "));
         System.out.println(findUnique(""));
         System.out.println(findUnique(null));
+        System.out.println(findUnique("AAABBBCCCDEF"));
+        System.out.println(findUnique("AAABBBCCCDEF234"));
+
     }
 
     public static String findUnique(String str) {
 
         String unique = "";
 
-        if (str == null || str.isEmpty() || str.isBlank()) {
+        try {
+            if (str == null || str.isEmpty() || str.isBlank()) {
 
-            throw new IllegalArgumentException("Input can not be null, empty, blank ");
-        }
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (str.indexOf(c) == str.lastIndexOf(c)) {
-                unique += c;
+                throw new IllegalArgumentException("Input can not be null, empty, blank ");
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (str.indexOf(ch) == str.lastIndexOf(ch)) {
+                    unique += ch;
+                }
             }
         }
-        return unique;
+            return unique;
+
+
     }
 }
