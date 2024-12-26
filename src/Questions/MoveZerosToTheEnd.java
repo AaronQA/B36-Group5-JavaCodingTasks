fd5dcbc27cb894adeffbb0bf34164b869b44ba76
aiturgank <48601244+aiturgank@users.noreply.github.com>
2024-12-26 09:37:06 -0500

    Accidentally solved under wrong package, I apologize

--- src/Questions/MoveZerosToTheEnd.java
+++ src/Questions/MoveZerosToTheEnd.java
@@ -1,33 +1,11 @@
 package Questions;
 
 
-import java.util.Arrays;
 
 public class MoveZerosToTheEnd {
     /*Write a method that can move all the zeros to  last indexes of the array (Do Not Use Sort Method)
     Ex:
     input:  {1,0,2,0,3,0,4,0};
     output: [1, 2, 3, 4, 0, 0, 0, 0]*/
-
-    public static void main(String[] args) {
-
-        System.out.println(Arrays.toString(moveZeros(1, 0, 2, 0, 3, 0, 4, 0)));
-
-    }
-
-    public static int[] moveZeros(int... arr) {
-
-        int[] result = new int[arr.length];
-        int index = 0;
-
-        for (int each : arr){
-            if(each != 0){
-                result[index++] = each;
-            }
-        }
-
-        return result;
-
-    }
 
 }
